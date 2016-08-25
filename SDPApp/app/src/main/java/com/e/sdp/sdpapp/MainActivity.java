538025d2128b1620c3c9126857fe753ed0b5732f
Ben <ben2117@me.com>
2016-08-25 11:12:34 +1000

    add firebase to app

--- SDPApp/app/src/main/java/com/e/sdp/sdpapp/MainActivity.java
+++ SDPApp/app/src/main/java/com/e/sdp/sdpapp/MainActivity.java
@@ -3,11 +3,20 @@ package com.e.sdp.sdpapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//make sure sdk tools are updated including google play services
+import com.google.firebase.database.*;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //writing to database
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
     }
 }
+
